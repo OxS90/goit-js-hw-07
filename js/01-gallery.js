e7c59cc65a634bc5c1aca8820bcb950b933fbb71
OxS90 <oxana.slivinschi@gmail.com>
2023-11-03 10:03:16 +0200

    commit_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,17 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-const galleryItems = document.querySelector(".gallery");
-function createGalleryItems(item) {
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
 	const galleryItem = document.createElement("li");
-	galleryItem.classList.add("gallery-item");
+	galleryItem.classList.add("gallery__item");
 
 	const galleryLink = document.createElement("a");
-	galleryLink.classList.add("gallery-link");
+	galleryLink.classList.add("gallery__link");
 	galleryLink.href = item.original;
 
 	const galleryImage = document.createElement("img");
-	galleryImage.classList.add("gallery-image");
+	galleryImage.classList.add("gallery__image");
 	galleryImage.src = item.preview;
 	galleryImage.alt = item.description;
 	galleryImage.setAttribute("data-source", item.original);
@@ -21,14 +21,19 @@ function createGalleryItems(item) {
 
 	return galleryItem;
 }
+
 galleryItems.forEach((item) => {
 	const galleryItem = createGalleryItem(item);
 	galleryList.appendChild(galleryItem);
 });
+
+let instance = null;
+
 galleryList.addEventListener("click", (event) => {
 	event.preventDefault();
-	if (event.target.nodeName === "img") {
+	if (event.target.nodeName === "IMG") {
 		const imageUrl = event.target.dataset.source;
+
 		instance = basicLightbox.create(
 			`<img src="${imageUrl}" width="800" height="600">`
 		);
@@ -36,9 +41,9 @@ galleryList.addEventListener("click", (event) => {
 		instance.show();
 	}
 });
+
 document.addEventListener("keydown", (event) => {
 	if (event.key === "Escape" && instance) {
 		instance.close();
 	}
 });
-console.log(galleryItems);
